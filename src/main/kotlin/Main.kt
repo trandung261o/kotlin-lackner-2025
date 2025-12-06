6c451461a1e979f887660e26b920a245fb25bb3d
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-06 18:10:19 +0700

    exception & try/catch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,27 +2,21 @@ fun main() {
     println("Please enter a number:")
     val input = readln()
 
-    val inputAsInteger = input.toIntOrNull()
+//    throw Exception("Dude, wtf did you pass there?!")
 
-    if (inputAsInteger != null) {
-//        val output = when {
-//            inputAsInteger % 2 == 0 -> "The number is even!"
-//            inputAsInteger < 10 -> "The number is odd and less than 10"
-//            else -> "The number is odd and at least 11"
-//        }
-
-        val output = when (inputAsInteger) {
-            null -> "Dude, enter a valid number!"
-            3 -> "The number is three!"
-            5 -> "The number is five!"
-            in 10..20 -> "The number is between 10 and 20"
-            else -> "I have no idea what to print"
-        }
-
-        println(output)
-
-    } else {
-        println("Dude, enter a valid number!")
+    val inputAsInteger = try {
+        input.toInt()
+    } catch (e: NumberFormatException) {
+        0
     }
 
+    val output = when (inputAsInteger) {
+        null -> "Dude, enter a valid number!"
+        3 -> "The number is three!"
+        5 -> "The number is five!"
+        in 10..20 -> "The number is between 10 and 20"
+        else -> "I have no idea what to print!"
+    }
+
+    println(output)
 }
