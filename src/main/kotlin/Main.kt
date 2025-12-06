8cae25716e01c5de604362c1b87b7261d8d36ff1
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-06 18:41:22 +0700

    arrays

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,22 @@
 fun main() {
-    println("Please enter a number:")
+    val favoriteNumbers = intArrayOf(1, 2, 3, 69)
+
+    favoriteNumbers[2] = 26
+
+    println(favoriteNumbers[0])
+    println(favoriteNumbers[1])
+    println(favoriteNumbers[2])
+    println(favoriteNumbers[3])
+    println(favoriteNumbers.getOrNull(4))
+
+    println("Enter your index: ")
     val input = readln()
+    val inputAsInteger = input.toIntOrNull()
 
-//    throw Exception("Dude, wtf did you pass there?!")
-
-    val inputAsInteger = try {
-        input.toInt()
-    } catch (e: NumberFormatException) {
-        0
+    if (inputAsInteger != null && inputAsInteger in 0..favoriteNumbers.lastIndex) {
+        println("Your number is ${favoriteNumbers[inputAsInteger]}")
+    } else {
+        println("That index doesn't exist")
     }
 
-    val output = when (inputAsInteger) {
-        null -> "Dude, enter a valid number!"
-        3 -> "The number is three!"
-        5 -> "The number is five!"
-        in 10..20 -> "The number is between 10 and 20"
-        else -> "I have no idea what to print!"
-    }
-
-    println(output)
 }
