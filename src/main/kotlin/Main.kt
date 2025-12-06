ff602c53e881ee8dbe6a2a1f9a5e6d10b82a72ca
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-06 21:54:04 +0700

    normal function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,50 +1,22 @@
 fun main() {
-    print("Amount of numbers: ")
-    val amountOfNumbers = readln().toIntOrNull() ?: 0
-
-    var i = 0
-    var sum = 0
-
-    val numbers1 = mutableListOf<Int>()
-    val numbers2 = mutableListOf<Int>()
-
-
-    while (i < amountOfNumbers) {
-        print("Number ${i+1}: ")
-        val number = readln().toIntOrNull() ?: continue
-        numbers1.add(number)
-        sum += number
-        i++
-    }
-
-    println("Loop with for case:")
-    var j = 0
-    for (j in 0 until amountOfNumbers) {
-        print("Number ${j+1}: ")
-        val number = readln().toIntOrNull() ?: continue
-        numbers2.add(number)
-    }
-
-    println("Your sum is: $sum")
-    println("Your numbers1 list is: $numbers1")
-
-    println("Your numbers2 list is:");
-    for (number in numbers2) {
-        println(number)
-    }
-
     print("Enter a string: ")
     val input = readln()
 
-    for (character in input) {
-        println(character)
-    }
+    val reversed = reversed(stringToReverse = input)    // = reversed(input)
 
+    println(reversed)
+
+    if (input == reversed) {
+        println("That's a palindrome!")
+    }
+}
+
+fun reversed(stringToReverse: String = "hello world!"): String {
     val finalString = buildString {
-        for (i in input.lastIndex downTo 0) {
-            append(input[i])
+        for (i in stringToReverse.lastIndex downTo 0) {
+            append(stringToReverse[i])
         }
     }
 
-    println(finalString)
+    return finalString
 }
