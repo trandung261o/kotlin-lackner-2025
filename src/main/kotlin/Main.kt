93fc8ee99dd11e72b17d32786e6751d9a5c13510
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-06 17:51:01 +0700

    when expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,24 +5,18 @@ fun main() {
     val inputAsInteger = input.toIntOrNull()
 
     if (inputAsInteger != null) {
-        val isEven = inputAsInteger % 2 == 0
-
-//        if (isEven) {
-//            println("The number is even")
-//        } else {
-//            println("The number is odd")
+//        val output = when {
+//            inputAsInteger % 2 == 0 -> "The number is even!"
+//            inputAsInteger < 10 -> "The number is odd and less than 10"
+//            else -> "The number is odd and at least 11"
 //        }
 
-//        if (!isEven) {
-//            println("The number is odd")
-//        }
-
-        val output = if (inputAsInteger % 2 == 0) {
-            "The number is even!"
-        } else if (inputAsInteger < 10) {
-            "The number is odd and less than 10!"
-        } else {
-            "The number is odd and at least 11!"
+        val output = when (inputAsInteger) {
+            null -> "Dude, enter a valid number!"
+            3 -> "The number is three!"
+            5 -> "The number is five!"
+            in 10..20 -> "The number is between 10 and 20"
+            else -> "I have no idea what to print"
         }
 
         println(output)
