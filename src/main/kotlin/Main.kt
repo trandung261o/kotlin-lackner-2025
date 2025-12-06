ea3561cf02a08a5d9c7cf64a817c5469fa1584bf
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-06 19:16:13 +0700

    loops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,50 @@
 fun main() {
-    val favoriteNumbers = intArrayOf(1, 2, 3, 69)
+    print("Amount of numbers: ")
+    val amountOfNumbers = readln().toIntOrNull() ?: 0
 
-    favoriteNumbers[2] = 26
+    var i = 0
+    var sum = 0
 
-    println(favoriteNumbers[0])
-    println(favoriteNumbers[1])
-    println(favoriteNumbers[2])
-    println(favoriteNumbers[3])
-    println(favoriteNumbers.getOrNull(4))
+    val numbers1 = mutableListOf<Int>()
+    val numbers2 = mutableListOf<Int>()
 
-    println("Enter your index: ")
-    val input = readln()
-    val inputAsInteger = input.toIntOrNull()
 
-    if (inputAsInteger != null && inputAsInteger in 0..favoriteNumbers.lastIndex) {
-        println("Your number is ${favoriteNumbers[inputAsInteger]}")
-    } else {
-        println("That index doesn't exist")
+    while (i < amountOfNumbers) {
+        print("Number ${i+1}: ")
+        val number = readln().toIntOrNull() ?: continue
+        numbers1.add(number)
+        sum += number
+        i++
     }
 
+    println("Loop with for case:")
+    var j = 0
+    for (j in 0 until amountOfNumbers) {
+        print("Number ${j+1}: ")
+        val number = readln().toIntOrNull() ?: continue
+        numbers2.add(number)
+    }
+
+    println("Your sum is: $sum")
+    println("Your numbers1 list is: $numbers1")
+
+    println("Your numbers2 list is:");
+    for (number in numbers2) {
+        println(number)
+    }
+
+    print("Enter a string: ")
+    val input = readln()
+
+    for (character in input) {
+        println(character)
+    }
+
+    val finalString = buildString {
+        for (i in input.lastIndex downTo 0) {
+            append(input[i])
+        }
+    }
+
+    println(finalString)
 }
