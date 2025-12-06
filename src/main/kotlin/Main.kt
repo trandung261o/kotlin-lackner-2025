44ebc16494dc575736cf6ac5c56d05b57d12102b
Tran Dinh Dung <trandungtk321@gmail.com>
2025-12-06 22:24:08 +0700

    function overloading

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main() {
     print("Enter a string: ")
-    val input = readln()
+    val input = readln().toInt()
+
+    println(input + input.reversed())
 
     val reversed = input.reversed()
 
@@ -20,3 +22,21 @@ fun String.reversed(): String {
 
     return finalString
 }
+
+//fun reversed(string: String): String {
+//    val finalString = buildString {
+//        for (i in string.lastIndex downTo 0) {    //for (i in lastIndex downTo 0) => cung chap nhan duoc
+//            append(string[i])
+//        }
+//    }
+//
+//    return finalString
+//}
+
+fun Int.reversed(): Int {
+    return this.toString().reversed().toInt()
+}
+
+//fun reversed(number: Int): Int {
+//    return number.toString().reversed().toInt()
+//}
